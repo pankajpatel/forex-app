b51617415c9af363b9698cfe0ce3bd3cb41699b7
pankajpatel <pankajpatel@users.noreply.github.com>
2020-04-23 10:02:25 +0000

    deploy: d8c059c714b4b7a6b0c5e0aa384420e98c74134c

---
+++ precache-manifest.11c9cd1b4c2b98f0d735218134fbb946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696133f858e47fdeb1bc8316e10e04ff",
+    "url": "/forex-app/index.html"
+  },
+  {
+    "revision": "5949de10ed80514e8ce8",
+    "url": "/forex-app/static/css/2.84ecba5d.chunk.css"
+  },
+  {
+    "revision": "4ddd94d946e9c9e72d99",
+    "url": "/forex-app/static/css/main.fd9da9be.chunk.css"
+  },
+  {
+    "revision": "5949de10ed80514e8ce8",
+    "url": "/forex-app/static/js/2.e1f89df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/forex-app/static/js/2.e1f89df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ddd94d946e9c9e72d99",
+    "url": "/forex-app/static/js/main.7cde9084.chunk.js"
+  },
+  {
+    "revision": "b29f66daa0e9bef43f80",
+    "url": "/forex-app/static/js/runtime-main.4140463d.js"
+  }
+]);
