5beb1ac655446023ce849786c0bba5a2c17fdf1f
Pankaj Patel <pankaj.patel@abtasty.com>
2022-06-16 13:48:07 +0200

    fix-tests

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from 'react-dom/client';
 import "currency-flags/dist/currency-flags.min.css";
 import "./index.css";
 
 import { App } from "./components/App/App";
-import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
